4b67db312fc49aacdb51e845a84db2ddf6f8e686
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-05-23 12:55:35 +0900

    added validates to the models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
+
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, inclusion: { in: RATINGS }, numericality: { only_integer: true }
 end
